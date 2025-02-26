1ed2b5a18af7a36f31169daa3f86dca252f2b935
Валерий Косков <koskov.v@yandex.ru>
2025-02-26 23:59:31 +0300

    Выполнил необходимые задания темы 2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, r, angle;
+            Console.WriteLine("'Перевод угла из градусов в радианы'");
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            Console.WriteLine("Ведите значение градусов");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите значение минут");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите значение секунд");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            Console.WriteLine("Значение угла в грудусах: {0} {1}' {2}''", a, b, c);
+            angle = a + b / 60 + c / 60 / 60;
+            r = angle * Math.PI / 180;
+            Console.WriteLine("Значение угла в радианах: {0}", r);
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
+        }
+    }
+}
