7ae0340a397927fbbcdabbc76c33a949844dc75e
Валерий Косков <koskov.v@yandex.ru>
2025-03-11 01:18:13 +0300

    Выполнил обязательные задания четвертой темы.

---
+++ Task4_4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Вводятся целые числа a>0, b<0. Найти a^b. Не использовать класс Math
+        {
+            int a, b;
+            double pov;
+            Console.WriteLine("'Найти a^b. числа a>0, b<0'");
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            Console.Write("Укажите натуральное число a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Укажите целое число b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            pov = 1;
+            if (b < 0)
+            {
+                for (int i = -1; i >= b; i--)
+                {
+                    pov = pov / a;
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= b; i++)
+                {
+                    pov = pov * a;
+                }
+            }
+            Console.WriteLine("{0}^{1} = {2}", a, b, pov);
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4_7/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Вводится n. Определить количество цифр в числе
+        {
+            int n, counter, Quotient;
+            Console.WriteLine("'Определить количество цифр в числе'");
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            Console.Write("Укажите целое число n: ");
+            Quotient = n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            if (n == 0)
+            {
+                Console.WriteLine("Количество цифр в числе {0}: 1.", n);
+                Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            }
+            else
+            {
+                counter = 0;
+                while (Quotient > 0)
+                {
+                    Quotient = Quotient / 10;
+                    ++counter;
+                }
+                Console.WriteLine("Количество цифр в числе {0}: {1}.", n, counter);
+                Console.WriteLine();//просто отделяю смысловые абзацы пустой строкой
+            }
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
+        }
+    }
+}
