f549d9be36104c3184e9e6d7fed379f2714482b5
Валерий Косков <koskov.v@yandex.ru>
2025-05-22 00:43:48 +0300

    Решены задания 11 темы.

---
+++ Task11_1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Task11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animal = new Animal[]
+            {
+                new Cat(),
+                new Cat(),
+                new Dog(),
+                new Cat()
+            };
+            foreach (Animal i in animal)
+            {
+                i.ShowInfo();
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { get; }
+        public abstract string Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} говорит {Say()}");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override string Name => "Кот";
+        public override string Say()
+        {
+            return "Мяу!";
+        }
+    }
+    class Dog : Animal
+    {
+        public override string Name => "Собака";
+        public override string Say()
+        {
+            return "Гав!";
+        }
+    }
+}
---
+++ Task11_2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable[] flyables = new IFlyable[]
+            {
+                new Bird(),
+                new Bird(),
+                new Airplane(),
+                new Bird()
+            };
+            foreach (IFlyable i in flyables)
+            {
+                Console.WriteLine(i.Fly());
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+    interface IFlyable
+    {
+        int MaxAltitude { get; }
+        string Fly();
+
+
+    }
+    class Bird : IFlyable
+    {
+        public string Name => "Птица";
+        public int MaxAltitude => 12000;
+        public string Fly()
+        {
+            return $"{Name}. Лечу на высоте {MaxAltitude} метров.";
+        }
+    }
+    class Airplane : IFlyable
+    {
+        public string Name => "Самолет";
+        public int MaxAltitude => 11275;
+        public int CountPassengers => 150;
+        public string Fly()
+        {
+            return $"{Name}. Лечу на высоте {MaxAltitude} метров. Везу {CountPassengers} пассажиров.";
+        }
+    }
+}
