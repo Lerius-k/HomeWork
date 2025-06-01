ef73d5e3d6d052c353b9ac0744a590d746514f53
Валерий Косков <koskov.v@yandex.ru>
2025-06-01 15:27:24 +0300

    Решено 2 задачи 14 темы

---
+++ Task14_1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task14_1
+{
+    internal class Program
+    /*Требования:
+Создайте список для хранения фамилий игроков (тип string).
+Реализуйте следующие операции:
+    Добавление игроков "Иванов", "Петров", "Сидоров" методом Add.
+    Вставка игрока "Козлов" на позицию с индексом 1 методом Insert.
+    Проверка наличия игрока "Петров" в команде методом Contains.
+    Удаление игрока "Сидоров" методом Remove.
+    Поиск индекса игрока "Козлов" методом IndexOf.
+    Сортировка списка по алфавиту методом Sort.
+    Выведите текущий состав команды в формате:
+       1. Иванов  
+       2. Козлов  
+       3. Петров 
+    Проверьте, пуст ли список (свойство Count), и очистите его методом Clear.*/
+    {
+        static void Main(string[] args)
+        {
+            List<string> players = new List<string>();
+
+            players.Add("Иванов");
+            players.Add("Петров");
+            players.Add("Сидоров");
+
+            players.Insert(1, "Козлов");
+
+            bool getPlayer = players.Contains("Петров");
+            Console.WriteLine($"Есть ли игрок \"Петров\" в команде? {getPlayer}");
+
+            players.Remove("Сидоров");
+
+            int sportPlayerIndex = players.IndexOf("Козлов");
+            Console.WriteLine($"Индекс игрока \"Козлов\": {sportPlayerIndex}");
+
+            players.Sort();
+
+            Console.WriteLine("\nТекущий состав команды:");
+            for (int i = 0; i < players.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {players[i]}");
+            }
+
+            bool emptyList = players.Count == 0 ? true : false;
+            Console.WriteLine($"Команда пуста? {emptyList}");
+
+            players.Clear();
+            Console.WriteLine($"\nКоличество игроков после очистки: {players.Count}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task14_2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task14_2
+{
+    internal class Program
+    {
+        /*Требования:
+    Создайте словарь и добавьте в него 3 товара:
+        Ноутбуки ("A001") — 10 шт.
+        Смартфоны ("B205") — 25 шт.
+        Наушники ("C307") — 15 шт.
+    Реализуйте следующие операции:
+        Проверьте наличие товара с артикулом "B205" (ContainsKey).
+        Обновите количество ноутбуков до 8 (продали 2 шт.) через индексатор.
+        Получите количество наушников безопасным способом (TryGetValue).
+        Увеличьте количество смартфонов на 5 (новый завоз).
+        Удалите наушники из инвентаря (Remove).
+    Выведите текущий инвентарь в формате:
+       Артикул: A001, Количество: 8  
+       Артикул: B205, Количество: 30  
+    Проверьте, пуст ли словарь (Count), затем полностью очистите инвентарь (Clear).*/
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> storeHouse = new Dictionary<string, int>();
+
+            storeHouse.Add("A001", 10);
+            storeHouse.Add("B205", 25);
+            storeHouse.Add("C307", 15);
+
+            bool hasSmartphone = storeHouse.ContainsKey("B205");
+            Console.WriteLine($"Есть ли товар с артикулом B205? {hasSmartphone}");
+            storeHouse["A001"] -= 2;
+
+            if (storeHouse.TryGetValue("C307", out int countHeadphones))
+            {
+                Console.WriteLine($"\nКоличество наушников: {countHeadphones}");
+            }
+
+            storeHouse["B205"] += 5;
+
+            storeHouse.Remove("C307");
+
+            Console.WriteLine("\nТекущий инвентарь:");
+            foreach (KeyValuePair<string, int> products in storeHouse)
+            {
+                Console.WriteLine($"Артикул: {products.Key}, Количество: {products.Value}");
+            }
+
+            bool productsList = storeHouse.Count == 0;
+            Console.WriteLine($"\nИнвентарь пуст? {productsList}");
+
+            storeHouse.Clear();
+            Console.WriteLine($"\nКоличество товаров после очистки: {storeHouse.Count}");
+            Console.ReadKey();
+        }
+    }
+}
