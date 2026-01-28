4d0e948589817049310bd0423cc109d5ce7cb311
Валерий Косков <koskov.v@yandex.ru>
2026-01-28 23:50:25 +0300

    Плагин работает.

--- Iteration3_Plug-in Revit/HelloRevit/MyFirstApp.cs
+++ Iteration3_Plug-in Revit/HelloRevit/MyFirstApp.cs
@@ -34,6 +34,41 @@ namespace HelloRevit
                 );
 
             panel.AddItem(button);
+            //создание второй панели
+            var panel2 = application.CreateRibbonPanel("ПИК-Привет", "SDK инструменты");
+            //Команда PickforDeletion позволяет выбрать некоторые элементы, а затем удалить их из документа.
+            var button1 = new PushButtonData(
+                "PickforDeletion",
+                "Выбрать и удалить",
+                "C:\\Users\\koskovvo\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2019\\C#course\\Selections.dll",
+                "Revit.SDK.Samples.Selections.CS.PickforDeletion"
+                );
+            //Команда PlaceAtPointOnWallFace позволяет выбрать точку на стене, а затем установить окно с фиксированным размером 36" x 48".
+            var button2 = new PushButtonData(
+                "PlaceAtPointOnWallFace",
+                "Вставить окно по точке",
+                "C:\\Users\\koskovvo\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2019\\C#course\\Selections.dll",
+                "Revit.SDK.Samples.Selections.CS.PlaceAtPointOnWallFace"
+                );
+            //Команда PlaceAtPickedFaceWorkplane позволяет выбрать грань, установить на ней рабочую плоскость, а затем выбрать точку на рабочей плоскости в центр для создания круга.
+            var button3 = new PushButtonData(
+                "PlaceAtPickedFaceWorkplane",
+                "Круг по плоскости",
+                "C:\\Users\\koskovvo\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2019\\C#course\\Selections.dll",
+                "Revit.SDK.Samples.Selections.CS.PlaceAtPickedFaceWorkplane"
+                );
+            //Команда SelectionDialog позволяет выбрать элемент и точку из диалога. После выбора точки элемент перемещается в выбранную точку.
+            var button4 = new PushButtonData(
+                "SelectionDialog",
+                "Переместить в точку",
+                "C:\\Users\\koskovvo\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2019\\C#course\\Selections.dll",
+                "Revit.SDK.Samples.Selections.CS.SelectionDialog"
+                );
+            panel2.AddItem(button1);
+            panel2.AddItem(button2);
+            panel2.AddItem(button3);
+            panel2.AddItem(button4);
+
             return Result.Cancelled;
         }
 
