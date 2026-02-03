590d666475ee2fb0a77bfc2d408859dc80c1ba3d
Валерий Косков <koskov.v@yandex.ru>
2026-02-04 00:50:09 +0300

    Девятое задание 3й итерации выполнено

---
+++ Iteration3_Plug-in Revit/CreateSection/Cmd.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using CreateSection.Abstractions;
+using CreateSection.Services;
+using CreateSection.ViewModels;
+using CreateSection.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CreateSection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            ServiceCollection services = new ServiceCollection();
+            services.AddSingleton<ExternalCommandData>(commandData);
+            services.AddSingleton<RevitTask>(new RevitTask());
+            services.AddSingleton<ISelectionService, SelectionService>();
+            services.AddSingleton<ISectionService, SectionService>();
+            services.AddSingleton<MainWindowViewModel, MainWindowViewModel>();
+            services.AddSingleton<MainWindow, MainWindow>();
+            var provider = services.BuildServiceProvider();
+
+            var mainWindow = provider.GetRequiredService<MainWindow>();
+
+            mainWindow.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Iteration3_Plug-in Revit/CreateSection/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,85 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CreateSection.Abstractions;
+using System.Threading.Tasks;
+
+namespace CreateSection.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private readonly ISelectionService _selectionService;
+        private readonly ISectionService _sectionService;
+        private readonly RevitTask _revitTask;
+        private string _sectionName = "Реверсный куллер";
+        private double _widthOffsetMm = 100;
+        private double _depthOffsetMm = 100;
+        private double _heightOffsetMm = 100;
+
+        public MainWindowViewModel(
+            ISelectionService selectionService,
+            ISectionService sectionService,
+            RevitTask revitTask
+            )
+        {
+            CreateSectionCommand = new AsyncRelayCommand(OnCreateSectionCommandExecute);
+            _selectionService = selectionService;
+            _sectionService = sectionService;
+            _revitTask = revitTask;
+        }
+
+        public string SectionName
+        {
+            get => _sectionName;
+            set => SetProperty(ref _sectionName, value);
+        }
+
+        public double WidthOffsetMm
+        {
+            get => _widthOffsetMm;
+            set => SetProperty(ref _widthOffsetMm, value);
+        }
+
+        public double DepthOffsetMm
+        {
+            get => _depthOffsetMm;
+            set => SetProperty(ref _depthOffsetMm, value);
+        }
+
+        public double HeightOffsetMm
+        {
+            get => _heightOffsetMm;
+            set => SetProperty(ref _heightOffsetMm, value);
+        }
+
+        public AsyncRelayCommand CreateSectionCommand { get; }
+
+        private async Task OnCreateSectionCommandExecute()
+        {
+            FamilyInstance familyInstance = _selectionService.PickFamilyInstance();
+            if (familyInstance == null)
+            {
+                return;
+            }
+
+            bool isCreated = await _revitTask.Run<bool>(app =>
+            _sectionService.CreateSection(
+                familyInstance,
+                WidthOffsetMm,
+                DepthOffsetMm,
+                HeightOffsetMm,
+                SectionName
+                ));
+
+            if (!isCreated)
+            {
+                TaskDialog.Show("Ошибка", "Что-то пошло не так");
+            }
+            else
+            {
+                TaskDialog.Show("Успех", "Разрезы созданы");
+            }
+        }
+    }
+}
